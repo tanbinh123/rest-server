34ab45b0f0e79e8d140219425a536cb75474a797
mhero <gdmarav374@gmail.com>
2019-12-01 16:54:30 -0500

    Add dummy auth for dev profile

--- src/main/java/com/mac/springboot/security/WebSecurityConfig.java
+++ src/main/java/com/mac/springboot/security/WebSecurityConfig.java
@@ -1,6 +1,10 @@
 package com.mac.springboot.security;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,9 +12,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.mac.springboot.util.Auth;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private Environment environment;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests().antMatchers("/").permitAll().antMatchers(HttpMethod.POST, "/login")
@@ -25,6 +34,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// Create a default account
-		auth.inMemoryAuthentication().withUser("admin").password("{noop}password").roles("ADMIN");
+		if (Arrays.asList(environment.getActiveProfiles()).contains("dev")) {
+			var dummyAuth = new Auth("admin", "password");
+			auth.inMemoryAuthentication().withUser(dummyAuth.getUsername()).password("{noop}" + dummyAuth.getPassword())
+					.roles("ADMIN");
+		}
 	}
 }
