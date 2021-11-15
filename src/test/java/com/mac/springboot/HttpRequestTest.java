fddfda1e8d07957b2f9c9a152fa766d33fa1fdb1
Marco <gdmarav374@gmail.com>
2021-11-15 01:15:06 +0100

    Upgrade junit

--- src/test/java/com/mac/springboot/HttpRequestTest.java
+++ src/test/java/com/mac/springboot/HttpRequestTest.java
@@ -6,8 +6,7 @@ import java.util.Optional;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -16,12 +15,10 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.mac.springboot.util.Auth;
 import com.mac.springboot.util.Util;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class HttpRequestTest {
 
--- src/test/java/com/mac/springboot/SmokeTest.java
+++ src/test/java/com/mac/springboot/SmokeTest.java
@@ -2,16 +2,13 @@ package com.mac.springboot;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.mac.springboot.controller.RestApiController;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class SmokeTest {
 
